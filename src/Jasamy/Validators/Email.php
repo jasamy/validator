d1b7b2d1467111371023f4fb99d5fb9f9ac0bace
Jason Byers <jas@webmuscle.co.uk>
2017-03-22 19:08:16 +0000

    add validator classes

---
+++ src/Jasamy/Validators/Email.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SimpleValidator\Validators;
+
+class Email extends Base
+{
+    public function execute(array $data)
+    {
+        if ($this->isFieldNotEmpty($data)) {
+            $parts = explode('@', $data[$this->field]);
+
+            if (count($parts) !== 2) {
+                return false;
+            }
+
+            $len = mb_strlen($parts[0]);
+            if ($len === 0 || $len > 64) {
+                return false;
+            }
+
+            $len = mb_strlen($parts[1]);
+            if ($len === 0 || $len > 255) {
+                return false;
+            }
+
+            if ($parts[0]{0} === '.') {
+                return false;
+            }
+
+            if (strpos($parts[0], '..') !== false || strpos($parts[1], '..') !== false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
