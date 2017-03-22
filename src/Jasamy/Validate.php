48094b8f843b70c50dd250696f8ce881b1318bec
Jason Byers <jas@webmuscle.co.uk>
2017-03-22 19:15:29 +0000

    sort public/private classes

--- src/Jasamy/Validate.php
+++ src/Jasamy/Validate.php
@@ -8,44 +8,41 @@ namespace Jasamy;
  */
 class Validate
 {
-    public function validate()
+    private $data = array();
+    private $errors = array();
+    private $validators = array();
+
+    public function __construct(array $data, array $validators)
     {
-        private $data = array();
-        private $errors = array();
-        private $validators = array();
+        $this->data = $data;
+        $this->validators = $validators;
+    }
 
-        public function __construct(array $data, array $validators)
-        {
-            $this->data = $data;
-            $this->validators = $validators;
-        }
+    public function execute()
+    {
+        $result = true;
 
-        public function execute()
-        {
-            $result = true;
-
-            foreach ($this->validators as $validator) {
-                if (! $validator->execute($this->data)) {
-                    $this->addError($validator->getField(), $validator->getErrorMessage());
-                    $result = false;
-                }
+        foreach ($this->validators as $validator) {
+            if (! $validator->execute($this->data)) {
+                $this->addError($validator->getField(), $validator->getErrorMessage());
+                $result = false;
             }
-
-            return $result;
         }
 
-        public function addError($field, $message)
-        {
-            if (! isset($this->errors[$field])) {
-                $this->errors[$field] = array();
-            }
+        return $result;
+    }
 
-            $this->errors[$field][] = $message;
+    public function addError($field, $message)
+    {
+        if (! isset($this->errors[$field])) {
+            $this->errors[$field] = array();
         }
 
-        public function getErrors()
-        {
-            return $this->errors;
-        }
+        $this->errors[$field][] = $message;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
     }
 }
