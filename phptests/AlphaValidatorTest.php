46f64a7fcd51159a3a63aae12209878f0b2341d5
Jason Byers <jas@webmuscle.co.uk>
2017-03-23 10:58:26 +0000

    added more tests

---
+++ phptests/AlphaValidatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Jasamy\Validators\Alpha;
+
+class AlphaValidatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testValidator()
+    {
+        $message = 'not uppercase';
+
+        $v = new Alpha('field', $message);
+
+        $this->assertEquals($message, $v->getErrorMessage());
+
+        $this->assertFalse($v->execute(array('field' => 'abc')));
+        $this->assertFalse($v->execute(array('field' => 'A123')));
+        $this->assertFalse($v->execute(array('field' => 'a123')));
+        $this->assertFalse($v->execute(array('field' => null)));
+
+        $this->assertTrue($v->execute(array('field' => 'ABC')));
+    }
+}
---
+++ phptests/UrlValidatorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Jasamy\Validators\Url;
+
+class UrlValidatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testValidator()
+    {
+        $message = 'not a valid url';
+
+        $v = new Alpha('field', $message);
+
+        $this->assertEquals($message, $v->getErrorMessage());
+
+        $this->assertFalse($v->execute(array('field' => 'google.com')));
+        $this->assertFalse($v->execute(array('field' => 'www.google.com')));
+        $this->assertFalse($v->execute(array('field' => 'http://www.google')));
+        $this->assertFalse($v->execute(array('field' => null)));
+
+        $this->assertTrue($v->execute(array('field' => 'http://www.google.com')));
+        $this->assertTrue($v->execute(array('field' => 'https://www.google.com')));
+        $this->assertTrue($v->execute(array('field' => 'http://google.com')));
+    }
+}
